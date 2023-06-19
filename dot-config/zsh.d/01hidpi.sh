f5d89c58ea31abe194ab17535963c99ec5f9dc3d
Ray <linxray@gmail.com>
2023-06-19 16:13:21 +0800

    feat: refine for hidpi

---
+++ dot-config/zsh.d/01hidpi.sh
@@ -0,0 +1,9 @@
+res=$(grep Xft.dpi ~/.Xresources | grep -Eo '[0-9]+')
+
+if [[ $res > 96 ]]; then
+  export GDK_SCALE=2
+  export GDK_DPI_SCALE=0.5
+else
+  export GDK_SCALE=1
+  export GDK_DPI_SCALE=1
+fi
