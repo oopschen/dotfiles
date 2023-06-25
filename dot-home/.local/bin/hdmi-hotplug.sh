73ba75428a5dd0d715f45cd0202fa245adc8337f
Ray <linxray@gmail.com>
2023-06-25 09:24:06 +0800

    feat: 1. update ploy bar settings 2. upgrade hdmi-hotplug.sh add dp name support

--- dot-home/.local/bin/hdmi-hotplug.sh
+++ dot-home/.local/bin/hdmi-hotplug.sh
@@ -1,11 +1,28 @@
 #!/usr/bin/env bash
+#
+
+# find hdmi name
+extern_status=
+for ds in hdmi- dp-
+do
+    extern_status=$(xrandr|  grep -Ei "\b$ds\b.+\bconnected\b")
+    if [[ ! -z "$extern_status" ]]; then
+        echo "Choose external display: $ds"
+        break
+    fi
+
+done
+
+if [[ -z "$extern_status" ]]; then
+    echo "No external display found!!!"
+    exit 1
+fi
 
 
-hdmi_status=$(xrandr|  grep -Ei "hdmi.+\bconnected\b")
-edp_status=$(xrandr|  grep -Ei "edp.+\bconnected\b")
+inter_status=$(xrandr|  grep -Ei "\bedp\b.+\bconnected\b")
 
-hdmi_name=$(echo "$hdmi_status" | cut -d ' ' -f 1)
-edp_name=$(echo "$edp_status" | cut -d ' ' -f 1)
+extern_name=$(echo "$extern_status" | cut -d ' ' -f 1)
+inter_name=$(echo "$inter_status" | cut -d ' ' -f 1)
 
 cmd_xrandr="/usr/bin/xrandr"
 display_mode=""
@@ -14,48 +31,48 @@ _m=$1
 mode="${_m:=none}"
 
 function display_extern() {
-    for edp_loop_name in $($cmd_xrandr | grep -iE "\bedp" | cut -d " " -f 1)
+    for inter_loop_name in $($cmd_xrandr | grep -iE "\bedp" | cut -d " " -f 1)
     do
-        $cmd_xrandr --output $edp_loop_name --off 
+        $cmd_xrandr --output $inter_loop_name --off 
     done
-    $cmd_xrandr --output $hdmi_name --primary --auto 
+    $cmd_xrandr --output $extern_name --primary --auto 
 
 }
 
 function display_local() {
-    for hdmi_loop_name in $($cmd_xrandr | grep -iE "\bhdmi" | cut -d " " -f 1)
+    for extern_loop_name in $($cmd_xrandr | grep -iE "\bhdmi" | cut -d " " -f 1)
     do
-        $cmd_xrandr --output $hdmi_loop_name --off
+        $cmd_xrandr --output $extern_loop_name --off
     done
-    $cmd_xrandr --output $edp_name --primary --auto 
+    $cmd_xrandr --output $inter_name --primary --auto 
 }
 
-if [[ ! -z "$hdmi_status" && ! -z "$edp_status" ]]; then
+if [[ ! -z "$extern_status" && ! -z "$inter_status" ]]; then
     case $mode in 
         mirror) 
-            $cmd_xrandr --output $hdmi_name --primary --auto \
-                --output $edp_name --auto --same-as $hdmi_name
-            display_mode="$hdmi_name[*],$edp_name[*]"
+            $cmd_xrandr --output $extern_name --primary --auto \
+                --output $inter_name --auto --same-as $extern_name
+            display_mode="$extern_name[*],$inter_name[*]"
             ;;
         off) 
             display_local
-            display_mode="$hdmi_name,$edp_name[*]"
+            display_mode="$extern_name,$inter_name[*]"
             ;;
         *)
-            $cmd_xrandr --output $hdmi_name --primary --auto \
-                --output $edp_name --auto --left-of $hdmi_name
-            display_mode="$hdmi_name[*],$edp_name"
+            $cmd_xrandr --output $extern_name --primary --auto \
+                --output $inter_name --auto --left-of $extern_name
+            display_mode="$extern_name[*],$inter_name"
             ;;
 
     esac
 
-elif [[ -z "$hdmi_status" && ! -z "$edp_status" ]]; then
+elif [[ -z "$extern_status" && ! -z "$inter_status" ]]; then
     display_local
-    display_mode="$edp_name[*]"
+    display_mode="$inter_name[*]"
 
-elif [[ ! -z "$hdmi_status" &&  -z "$edp_status" ]]; then
+elif [[ ! -z "$extern_status" &&  -z "$inter_status" ]]; then
     display_extern
-    display_mode="$hdmi_name[*]"
+    display_mode="$extern_name[*]"
 
 else
     exit 1
