be6ab06eeb07805afdee447aff5630a3714774d0
Ray <linxray@gmail.com>
2023-07-11 12:32:09 +0800

    fix: container as cmd command error

--- dot-home/.local/populate-dotfiles.sh
+++ dot-home/.local/populate-dotfiles.sh
@@ -9,7 +9,7 @@ src_dir=$(realpath $(pwd)/$script_dir/bin)
 mkdir -p $dest_dir
 
 [[ ! -L  $dest_dir/auto-rip ]] && ln -sv $src_dir/auto_rip.sh $dest_dir/auto-rip
-[[ ! -L  $dest_dir/container_as_cmd ]] && ln -sv $src_dir/container_as_cmd.sh $dest_dir/container_as_cmd 
+[[ ! -L  $dest_dir/container_as_cmd ]] && ln -sv $src_dir/container_run_as_cmd.sh $dest_dir/container_as_cmd 
 [[ ! -L  $dest_dir/hdmi-hotplug.sh ]] && ln -sv $src_dir/hdmi-hotplug.sh $dest_dir/
 [[ ! -L  $dest_dir/i3-move-position.sh ]] && ln -sv $src_dir/i3-move-position.sh $dest_dir/
 [[ ! -L  $dest_dir/i3-utils-cmd-term.sh ]] && ln -sv $src_dir/i3-utils-cmd-term.sh $dest_dir/
