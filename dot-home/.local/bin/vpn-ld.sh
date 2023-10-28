214d5e4b6beb3c9ff867ce6a7471b23e32846662
Ray <linxray@gmail.com>
2023-10-28 20:13:56 +0800

    feat: 1. add vpn-ld to populate.sh 2. make vpn-ld more robust by adding status checking

--- dot-home/.local/bin/vpn-ld.sh
+++ dot-home/.local/bin/vpn-ld.sh
@@ -2,31 +2,84 @@
 
 mode=$1
 rcs="sudo rc-service"
+vpn_name=vpn-ld
+loop_max=6
+
+function add_routes() {
+    routes="192.168.70.0/24 192.168.254.0/24"
+    for r in $routes
+    do
+        sudo ip r add $r dev $1
+    done
+
+}
 
 case $mode in
     start)
         $rcs ipsec start
         $rcs xl2tpd start
-        sudo ipsec up vpn-ld
-        sudo xl2tpd-control connect-lac vpn-ld
-        sleep 5
 
-        routes="192.168.70.0/24 192.168.254.0/24"
-        interface_name=$(ip l | grep ppp | head -n 1 |  cut -d ':' -f 2 | tr -d ' ')
-        for r in $routes
+        sleep 1
+        sudo ipsec up $vpn_name > /dev/null
+        ## check status
+        loop_i=0
+        while true
         do
-            sudo ip r add $r dev $interface_name
+            output_status=$(sudo ipsec status $vpn_name | grep -i $vpn_name | grep -i establi)
+            if [ -z "$output_status" ]; then
+                sleep 1
+                sudo ipsec up $vpn_name > /dev/null
+                loop_i=$(echo "$loop_i + 1" | bc)
+                if [ $loop_max -ge $loop_i ]; then
+                    continue
+                else
+                    echo "ipsec tunnel up fails."
+                    exit 1
+                fi
+            fi
+
+            break
         done
 
+        
+        sudo xl2tpd-control connect-lac $vpn_name
+        ## chcek xl2tpd connection status
+        loop_i=0
+        while true
+        do
+            output_ppp=$(ip a | grep ppp | grep inet)
+            if [ -z "$output_ppp" ]; then
+                sleep 1
+                loop_i=$(echo "$loop_i + 1" | bc)
+                if [ $loop_max -ge $loop_i ]; then
+                    continue
+                else
+                    echo "xl2tpd connect lac fails."
+                    exit 2
+                fi
+            fi
+
+            break
+        done
+
+        interface_name=$(ip l | grep ppp | head -n 1 |  cut -d ':' -f 2 | tr -d ' ')
+        add_routes $interface_name
+
+        ip a
         ;;
     stop)
-        sudo xl2tpd-control disconnect-lac vpn-ld
-        sudo ipsec down vpn-ld
+        sudo xl2tpd-control disconnect-lac $vpn_name
+        sudo ipsec down $vpn_name
         $rcs xl2tpd stop
         $rcs ipsec stop 
         ;;
+        
+    route)
+        interface_name=$(ip l | grep ppp | head -n 1 |  cut -d ':' -f 2 | tr -d ' ')
+        add_routes $interface_name
+        ;;
 
     *)
-        echo "start | stop"
+        echo "start | stop | route"
         ;;
 esac
--- dot-home/.local/populate-dotfiles.sh
+++ dot-home/.local/populate-dotfiles.sh
@@ -21,3 +21,4 @@ mkdir -p $dest_dir
 [[ ! -L  $dest_dir/i3-utils-base-term.sh ]] && ln -sv $src_dir/i3-utils-base-term.sh $dest_dir/
 [[ ! -L  ~/.config/nftables.d ]] && ln -sv $src_dir/.nft/nftables.d ~/.config
 [[ ! -L  $dest_dir/polybar-vpn.sh ]] && ln -sv $src_dir/polybar-vpn.sh $dest_dir/
+[[ ! -L  $dest_dir/vpn-ld.sh ]] && ln -sv $src_dir/vpn-ld.sh $dest_dir/
