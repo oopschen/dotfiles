779e7bdccd4cc3ad349be9bbea101b7f2c135ed7
Ray <linxray@gmail.com>
2024-01-17 11:19:33 +0800

    feat: 1. add ld git cert 2. add misc file type to glfs

--- dot-home/.local/bin/init-glfs-doc.sh
+++ dot-home/.local/bin/init-glfs-doc.sh
@@ -4,8 +4,9 @@
 pic_files="*.jpg  *.png  *.jpeg *.gif *.webp"
 archive_files="*.zip  *.rar  *.7z *.tar *.tar.* *.ar *.iso *.gz *.lz *.jar"
 office_files="*.ppt *.pptx *.doc *.docx *.xls *.xlsx *.et *.wps *.pdf"
+misc_files="*.xmind"
 
-track_file_list="$pic_files $archive_files $office_files"
+track_file_list="$pic_files $archive_files $office_files $misc_files"
 
 for fl in $track_file_list
 do
