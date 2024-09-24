3efb6ec38e2a5139a10d7fba41bbb6812e15b6fc
Ray <linxray@gmail.com>
2024-09-24 23:43:40 +0800

    fix:1. ignore file for rg & fd

--- dot-config/zsh.d/20fzf.sh
+++ dot-config/zsh.d/20fzf.sh
@@ -1,20 +1,11 @@
-ignore_rules=("node_modules" "target" "build")
+# respect ignore in global .gitignore .rgignore .fdignore files
 
-rg_ignores=
-fd_ignores="! -ipath './.*'"
-for ig in "${ignore_rules[@]}"
-do
-    rg_ignores=" $rg_ignores --iglob='!$ig'"
-    fd_ignores="$fd_ignores ! -ipath './*$ig*'"
-done
-
-base_opts="-L -i"
-export FZF_DEFAULT_COMMAND="rg --files $base_opts $rg_ignores"
+export FZF_DEFAULT_COMMAND="rg --files -L -i"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_ALT_C_COMMAND="find . $fd_ignores -type d -print"
+export FZF_ALT_C_COMMAND="fd --type d -L"
 
-export FZF_DEFAULT_OPTS="--history=$HOME/.fzf_history --history-size=10000 --cycle -m"
-export FZF_CTRL_T_OPTS="--preview 'fzf-preview.sh {}'"
-export FZF_ALT_C_OPTS="--preview 'tree -C -L 7 {}'"
+export FZF_DEFAULT_OPTS="--history=$HOME/.fzf_history --history-size=10000 --cycle --wrap -m --no-mouse --tabstop=4"
+export FZF_CTRL_T_OPTS="--preview 'fzf-preview.sh {}' --preview-window='right,60%'"
+export FZF_ALT_C_OPTS="--preview 'tree -C -L 7 {}' --preview-window='right,60%'"
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
