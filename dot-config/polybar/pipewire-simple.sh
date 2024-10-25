9f0559b15eef326144398ec87e373088adfcff2e
Ray <linxray@gmail.com>
2024-10-25 17:40:30 +0800

    feat: bugifx pipewire volume script

--- dot-config/polybar/pipewire-simple.sh
+++ dot-config/polybar/pipewire-simple.sh
@@ -4,8 +4,6 @@
 #    defaultSink=$(wpctl status | awk '/^Audio/ {flag=1; next} /Sinks:/ && (flag == 1) {flag=2; next}  /Sources:/ && (flag==2){flag = 0 } flag==2 {print $0}')
 #}
 
-MUTE_STATE=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -i muted)
-
 case $1 in
     "--up")
         wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%+
@@ -13,16 +11,18 @@ case $1 in
     "--down")
         wpctl set-volume @DEFAULT_AUDIO_SINK@ 10%-
         ;;
-    "--mute")
+    "--toggle")
         wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
         ;;
 esac
 
+MUTE_STATE=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -i muted)
+VOLUME=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | sed -r 's/.*([0-9]\.[0-9]+).*/\1/ig' | tr -d ' ')
+#SINK=$(getDefaultSink)
+VOLUME_PERCENTAGE="$(echo "($VOLUME * 100)/1" | bc)%" 
+
 if [ -z "$MUTE_STATE" ]; then
-    VOLUME=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | sed -r 's/.*([0-9]\.[0-9]+).*/\1/ig' | tr -d ' ')
-    #SINK=$(getDefaultSink)
-    VOLUME_PERCENTAGE="$(echo "($VOLUME * 100)/1" | bc)%" 
     echo -e "%{T2}\UF028%{T-} %{T1}${VOLUME_PERCENTAGE}%{T-}"
 else
-    echo -e "%{F#A54242}%{T2}\UF075F%{T-}%{F-}"
+    echo -e "%{F#A54242}%{T2}\UF075F%{T-} %{T1}${VOLUME_PERCENTAGE}%{T-}%{F-}"
 fi
