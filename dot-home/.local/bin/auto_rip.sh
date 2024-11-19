05268c76bb122faf73161bd7c652a9d9c8a1d22d
Ray <linxray@gmail.com>
2024-11-19 18:48:38 +0800

    fix: auto rip error check

--- dot-home/.local/bin/auto_rip.sh
+++ dot-home/.local/bin/auto_rip.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 
 destfile=/home/work/.config/.shadowsocks-redir-ignore
+tmpdestfile=/tmp/.shadowsocks-redir-ignore.tmp
 downloadfile=/tmp/apnic-lastest
+timeouts_sec=300
 
-curl -L -o $downloadfile "http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest"
+curl -L --connect-timeout $timeouts_sec -m $timeouts_sec \
+    -o $downloadfile "http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest"
 
-if [ 0 -ne $? ]; then
+if [ 0 -ne $? ] || [ 0 -eq $(wc -l $downloadfile | cut -d ' ' -f 1) ]; then
   echo "Download latest apnic fail"
   exit 1
 fi
 
 ripquerier -i $downloadfile -t ipv4 -s allocated -s assigned -c HK -c CN \
-  | tail -n +5 > $destfile
+  | tail -n +5 > $tmpdestfile
+
+if [ 0 -eq $(wc -l $tmpdestfile | cut -d ' ' -f 1) ]; then
+    echo "No entry found."
+    exit 2
+fi
+
+mv $tmpdestfile $destfile
