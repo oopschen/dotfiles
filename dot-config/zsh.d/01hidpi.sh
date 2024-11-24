cd2bb73301a4044556a14ab993a1ef9e4218ae06
Ray <linxray@gmail.com>
2024-11-24 11:00:25 +0800

    feat: add qt hidpi settings

--- dot-config/zsh.d/01hidpi.sh
+++ dot-config/zsh.d/01hidpi.sh
@@ -3,6 +3,8 @@ res=$(grep Xft.dpi ~/.Xresources | grep -Eo '[0-9]+')
 if [[ $res -gt 96 ]]; then
   export GDK_SCALE=2
   export GDK_DPI_SCALE=0.5
+  # qt 5 
+  export QT_ENABLE_HIGHDPI_SCALING=1
 else
   export GDK_SCALE=1
   export GDK_DPI_SCALE=1
